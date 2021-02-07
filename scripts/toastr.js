6d9b6489f5f69a2abcb29968282b0f0fce0eb704
Siyuan Huo <georgehuo0211@gmail.com>
2021-02-07 14:06:07 -0500

    add send email function but comment

---
+++ scripts/toastr.js
@@ -0,0 +1,18 @@
+toastr.options = {
+  "closeButton": false,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": false,
+  "positionClass": "toast-top-center",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": 0,
+  "extendedTimeOut": 0,
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut",
+  "tapToDismiss": false
+}
